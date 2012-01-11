00e0d2487145a2f36df209a6befc3b16b307d004
Christopher Kaster <kasoki@arcor.de>
2012-01-11 15:37:58 +0100

    Fixed a bug in provider.js

--- src/provider.js
+++ src/provider.js
@@ -3,42 +3,48 @@
 		"key": "dan",
 		"name": "danbooru",
 		"type": "danbooru",
-		"url": "http://danbooru.donmai.us",
+		"url": "http://danbooru.donmai.us"
 	},
 	{
 		"key": "scomplex_chan",
 		"name": "Sankaku Complex (chan)",
 		"type": "danbooru",
-		"url": "http://chan.sankakucomplex.com",
+		"url": "http://chan.sankakucomplex.com"
 	},
 	{
 		"key": "scomplex_idol",
 		"name": "Sankaku Complex (idol)",
 		"type": "danbooru",
-		"url": "http://idol.sankakucomplex.com",
+		"url": "http://idol.sankakucomplex.com"
 	},
 	{
 		"key": "imouto",
 		"name": "Ore no Imouto",
 		"type": "danbooru",
-		"url": "http://oreno.imouto.org",
+		"url": "http://oreno.imouto.org"
 	},
 	{
 		"key": "konachan",
 		"name": "Konachan",
 		"type": "danbooru",
-		"url": "http://konachan.com",
+		"url": "http://konachan.com"
 	},
 	{
 		"key": "gel",
 		"name": "gelbooru",
 		"type": "gelbooru",
-		"url": "http://gelbooru.com",
+		"url": "http://gelbooru.com"
 	},
 	{
 		"key": "rule34",
 		"name": "Rule34",
 		"type": "gelbooru",
-		"url": "http://rule34.booru.org",
+		"url": "http://rule34.booru.org"
+	},
+	{
+		"key": "safe",
+		"name": "Safebooru",
+		"type": "gelbooru",
+		"url": "http://safebooru.org"
 	}
 ]
