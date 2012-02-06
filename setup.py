30ead9bba486c8fa5b0f171d93ad6d1c0c66f646
Nooby <Nooby@gmx.eu>
2012-02-06 05:23:03 +0100

    Installation with setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+setup(
+    name='BooruPy',
+    version='0.1.2',
+    keywords = ['pictures', 'anime', 'booru', 'gelbooru', 'danbooru'],
+    author='Christopher Kaster',
+    author_email='ikasoki@gmail.com',
+    maintainer='Giuliano Di Pasquale',
+    maintainer_email='gdipasquale@gmx.de',
+    packages=['BooruPy'],
+    url='http://github.com/Kasoki/BooruPy',
+    license='GNU GENERAL PUBLIC LICENSE',
+    description='BooruPy is a simple library for interacting with various image board "Booru" systems.',
+    long_description=open('README.rst').read(),
+    platforms='OS Independent',
+    classifiers= [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries'
+    ]
+)
