19cf9c8627edc8f1ec3fbd9f3088e937149323a5
Kasoki <ikasoki@gmail.com>
2012-02-28 19:58:40 +0100

    pep8 style corrections

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from distutils.core import setup
 setup(
     name='BooruPy',
     version='0.1.5',
-    keywords = ['pictures', 'anime', 'booru', 'gelbooru', 'danbooru', 'image',
-     'board', 'imageboard'],
+    keywords=['pictures', 'anime', 'booru', 'gelbooru', 'danbooru', 'image',
+        'board', 'imageboard'],
     author='Christopher Kaster',
     author_email='ikasoki@gmail.com',
     maintainer='Giuliano Di Pasquale',
@@ -12,10 +12,11 @@ setup(
     packages=['BooruPy'],
     url='http://github.com/Kasoki/BooruPy',
     license='GNU GENERAL PUBLIC LICENSE',
-    description='BooruPy is a simple library for interacting with various image board "Booru" systems.',
+    description='BooruPy is a simple library for interacting ' +
+        'with various image board "Booru" systems.',
     long_description=open('README.rst').read(),
     platforms='OS Independent',
-    classifiers= [
+    classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
